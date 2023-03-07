6b06852bf739aa6edaa2d7452ad1051b17df6dea
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-03-07 08:27:25 -0500

    nuevaSolucion

--- solucion.js
+++ solucion.js
@@ -1,17 +1,18 @@
-const notas = {
-    4: "Exceeds proficiency",
-    3: "Demonstrates profiencicy",
-    2: "Approaches profiencicy",
-    1: "Falls well below profiencicy",
-    0: ["Lacks all proficiency", "No attempt made"]
-}
-
-
-
-let ingresoNota = prompt("Por favor ingresa su nota");
-for (const key in notas) {
-    if (ingresoNota === key) {
-        console.log(notas[key]);
-        
+const notas = [
+    { equivalencia: "A", mensaje: "Exceeds proficiency", rango: [12, 14] },
+    { equivalencia: "B", mensaje: "Demonstrates profiencicy", rango: [9, 11] },
+    { equivalencia: "C", mensaje: "Approaches profiencicy", rango: [6, 8] },
+    { equivalencia: "D", mensaje: "Falls well below profiencicy", rango: [3, 5] },
+    { equivalencia: "E", mensaje: "Lacks all proficiency", rango: [1, 2] },
+  ];
+  
+  let ingresoNota = parseInt(prompt("Por favor ingresa su nota"));
+  let calificacion = "";
+  notas.forEach((element) => {
+    if ( ingresoNota >= element.rango[0] && ingresoNota<=element.rango[1]) {
+          calificacion = element.equivalencia;
     }
-}
+  });
+  
+  let rta=notas.find(nota=>nota.equivalencia===calificacion)
+  console.log(`Su calificacion es ${rta.equivalencia} ${rta.mensaje}`);
